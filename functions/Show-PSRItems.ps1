aaf27ad2ea4a53b799b13cf775ec81ac5521f8a4
Jon Chery <jchery@outlook.com>
2018-04-25 11:58:26 -0400

    fixed bug where variable PSRegistryDbPath was defined as a local vs environment scope

--- functions/Show-PSRItems.ps1
+++ functions/Show-PSRItems.ps1
@@ -3,7 +3,7 @@ Function Show-PSRItems {
         $Registry=$env:psr_registry
     )
 
-    $RegistryPath = Join-Path -Path $PSRegistryDbPath -ChildPath ("$Registry" + ".psr")
+    $RegistryPath = Join-Path -Path $env:PSRegistryDbPath -ChildPath ("$Registry" + ".psr")
     $Psr = Import-Clixml -Path $RegistryPath | Format-List
     Return $Psr
 }
